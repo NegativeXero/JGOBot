35f0a30e9cd9c6aef6cfd839c322e7f250b77c31
Xero <5c0jt.c@gmail.com>
2015-05-29 20:43:42 +1200

    Initial Commit

---
+++ src/org/javagaming/bot/Command.java
@@ -0,0 +1,21 @@
+package org.javagaming.bot;
+
+public abstract class Command {
+	public final String COMMAND_NAME;
+	public final String[] ALIASES;
+	public String description;
+	public String[] usage;
+	public boolean hidden = false;
+	JGOBot bot;
+	public Command(JGOBot bot, String commandName) {
+		this(bot, commandName, null);
+	}
+	
+	public Command(JGOBot bot, String commandName, String[] aliases) {
+		this.bot = bot;
+		this.COMMAND_NAME = commandName;
+		this.ALIASES = aliases;
+	}
+	
+	public abstract void execute(String sender, String[] commandArgs);
+}
---
+++ src/org/javagaming/bot/JGOBot.java
@@ -0,0 +1,82 @@
+package org.javagaming.bot;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jibble.pircbot.IrcException;
+import org.jibble.pircbot.PircBot;
+
+public class JGOBot extends PircBot {
+
+	final static String NETWORK = "irc.freenode.net";
+	final static String CHANNEL = "#jgobot";
+	//final static String CHANNEL = "#java-gaming";
+	
+	final static String COMMAND_IDENTIFIER = "::";
+	final static String BOT_NAME = "JGOBot";
+	
+	private List<Command> commands = new ArrayList<Command>();
+	
+	public static void main(String[] args) {
+		JGOBot bot = new JGOBot();
+		bot.setVerbose(true);
+		try {
+			bot.connect(NETWORK);
+		} catch (IOException | IrcException e) {
+			System.err.println("Failed to join freenode. Exitting now.");
+			e.printStackTrace();
+		}
+		bot.joinChannel(CHANNEL);
+	}
+	
+	public JGOBot() {
+		//Set the bot's nickname
+		this.setName(BOT_NAME);
+		registerCommand(new HelpCommand(this));
+	}
+	
+	private void registerCommand(Command command) {
+		commands.add(command);
+	}
+	
+	//Event triggered by a message being sent in the channel
+	public void onMessage(String channel, String sender, 
+			String login, String hostname, String message) {
+		if(message.startsWith(COMMAND_IDENTIFIER) && !sender.equalsIgnoreCase(BOT_NAME)) {
+			String[] commandArgs = message.split(" ");
+			commandArgs[0] = commandArgs[0].replaceAll(COMMAND_IDENTIFIER, "");
+			getCommand(commandArgs[0]).execute(sender, commandArgs);
+		}
+	}
+	
+	public Command getCommand(String command) {
+		for(Command c : commands){
+			if(command.equalsIgnoreCase(c.COMMAND_NAME)) {
+				return c;
+			}
+		}
+		for(Command c : commands) {
+			for(String s : c.ALIASES) {
+				if(command.equalsIgnoreCase(s)) {
+					return c;
+				}
+			}
+		}
+		return null;
+	}
+	
+	public String[] getCommands() {
+		String[] commandList = new String[commands.size()];
+		int i = 0;
+		for(Command command : commands) {
+			commandList[i] = command.COMMAND_NAME;
+			i++;
+		}
+		return commandList;
+	}
+
+	public void send(String message){
+		this.sendMessage(JGOBot.CHANNEL, message);
+	}
+}
